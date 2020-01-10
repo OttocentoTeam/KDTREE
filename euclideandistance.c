bd81664dbca2095ab1b9a705532d3e3d967ab860
Tommaso Ruga <tommaso@MacBook-di-Tommaso.local>
2020-01-10 12:45:36 +0100

    k dimension eu dist

--- euclideandistance.c
+++ euclideandistance.c
@@ -2,48 +2,12 @@
 #include <stdlib.h>
 #include <math.h>
 
-float EuclideanDistance(Punto *p, Punto *q);
 
-typedef struct point {
-
-        int x,
-            y;
-
-} Punto; 
-
-float EuclideanDistance(Punto *p, Punto *q) { //metodo per il calcolo della distanza tra due punti
-
-      return sqrt((q->x - p->x)*(q->x - p->x) + (q->y - p->y)*(q->y - p->y)); //radice quadrata di (qx-px)^2 + (qy-qy)^2
+float EuclideanDistance(float *p, float *q) { //metodo per il calcolo della distanza tra due punti
+      float somma=0;
+      for(int i=0, i<k, i++){
+            int var = ((q+i)-(p+i))*((q+i)-(p+i)); //differenza delle k dimensioni ed elevamento al 2
+            somma+=var;
+      }
+      return sqrt(somma);
 }
-
-int main() { //main di prova 
-
-    Punto *p,
-          *q;
-   
-    p = (Punto*)malloc(sizeof(Punto));
-    q = (Punto*)malloc(sizeof(Punto));
-     
-    float res;
- 
-    printf("Primo punto\n");
-    printf("x: ");
-    scanf("%d", &p->x); 
-    printf("y: ");
-    scanf("%d", &p->y); 
-
-    printf("Secondo punto\n");
-    printf("x: ");
-    scanf("%d", &q->x); 
-    printf("y: ");
-    scanf("%d", &q->y); 
-
-    res = EuclideanDistance(p,q);
-    printf("Distanza = %f", res);
-
-    free(p);
-    free(q);
-
- return(0);
-
-};
